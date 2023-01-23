cb1ac46fcd63b5cc4e46acc3cda6179133df8f56
felixbor <felixbor@gmail.com>
2023-01-22 21:42:13 -0400

    debugging get request in apiroute

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -3,28 +3,25 @@ const router = require('express').Router();
 // adding uuid module to create unique id  for each note
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
-const db = require('../db/db.json');
+//const db = require('../db/db.json');
 const fs = require('fs');
 
 router.get('/notes', (req, res) => {
     console.log('here')
   fs.readFile("db/db.json", (err, data) => {
+    console.log(JSON.parse(data))
     res.json(JSON.parse(data))    
   })
 })
-/*
-router.get('/notes', (req, res) => {
-        readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
-      });
-*/
+ 
 router.post('/notes', (req, res) => {
         console.log(req.body);
-    
+      let db =JSON.parse(fs.readFileSync('db/db.json',{encoding:"utf8"}))
         db.push(req.body);
+        console.log(db)
         //adding unique identifier
         req.body.id = uuidv4();
-        fs.writeFileSync(
-            path.join(__dirname, '../db/db.json'),
+        fs.writeFileSync( 'db/db.json',
             JSON.stringify(db , null, 2)
         );
     
@@ -33,12 +30,15 @@ router.post('/notes', (req, res) => {
 
     router.delete('/notes/:id', (req, res) => {
         // reading notes form db.json
-        let notes = JSON.parse(fs.readFileSync('db/db.json'))
+        let notes =JSON.parse(fs.readFileSync('db/db.json',{encoding:"utf8"}))
+        //console.log(typeof notes)
+       // console.log(notes)
         // removing note with id
-        let deleteNotes = notes.filter(item => item.id !== req.params.id);
-        // Rewriting note to db.json
-        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-        res.json(deleteNotes);
+        let updatedNotes = notes.filter(item => item.id !== req.params.id);
+       // console.log(updatedNotes)
+        // Rewriting/saving filtered  notes to db.json
+        fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes));
+        res.json(updatedNotes);
 
         })
 
